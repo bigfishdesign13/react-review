6fa9e360dc41c591a2b9758b3cd5c1d38dac9007
bigfishdesign13 <marty@brutvan.com>
2025-03-09 21:28:39 -0400

    added successful form submit handling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,102 @@
-import { useContext, useEffect, useState } from "react";
-import TextInput from "../components/TextInput";
-import Heading from "../components/Heading";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Alert, Button, ButtonGroup, HStack, VStack } from "@chakra-ui/react";
+import { CloseButton } from "../components/ui/close-button";
 import { Container } from "../App.styles";
+import Heading from "../components/Heading";
+import TextInput from "../components/TextInput";
 import DataContext from "../utils/DataContext";
-import { Button, HStack, VStack } from "@chakra-ui/react";
 
-function Home() {
+const Home = () => {
   const { firstName, setFirstName, lastName, setLastName } =
     useContext(DataContext);
 
   const [isInvalid, setIsInvalid] = useState({
-    firatName: false,
+    firstName: false,
     lastName: false,
   });
-  useEffect(() => {
-    console.log(`Full name: ${lastName}, ${firstName}`);
-  }, [firstName, lastName]);
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [validSubmit, setValidSubmit] = useState(false);
+
+  const alertRef = useRef(null);
+
+  const navigate = useNavigate();
+
   const onChangeHandlerFN = (e) => {
     const val = e.target.value;
     setFirstName(val);
-    // console.log(`val == ${val}`);
   };
   const onChangeHandlerLN = (e) => {
     const val = e.target.value;
     setLastName(val);
-    // console.log(`val == ${val}`);
   };
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler = () => {
     setIsInvalid({
       firstName: firstName === "",
       lastName: lastName === "",
     });
+    setSubmitted(true);
   };
+  const onClearHandler = () => {
+    // setValidSubmit(false);
+    setFirstName("");
+    setLastName("");
+    // setIsInvalid({
+    //   firstName: false,
+    //   lastName: false,
+    // });
+    setShowAlert(false);
+  };
+  const onClickClose = (e) => {
+    setShowAlert(false);
+  };
+
+  // Show alert if there are form errors
+  useEffect(() => {
+    console.log(submitted);
+    submitted &&
+      (isInvalid.firstName || isInvalid.lastName
+        ? setShowAlert(true)
+        : setValidSubmit(true));
+  }, [submitted, isInvalid]);
+
+  // Focus on alert box when there is an error
+  useEffect(() => {
+    alertRef.current?.focus();
+  }, [showAlert]);
+
+  // Goto form
+  useEffect(() => {
+    validSubmit && navigate("/form");
+  }, [navigate, validSubmit]);
+
+  const AlertBox = (
+    <Alert.Root mt="2rem" ref={alertRef} status="error" tabIndex={1}>
+      <Alert.Indicator />
+      <Alert.Content>
+        <Alert.Title>Invalid Fields</Alert.Title>
+        <Alert.Description>
+          Your form has some errors. Please fix them and try again.
+        </Alert.Description>
+      </Alert.Content>
+      <CloseButton
+        pos="relative"
+        top="-2"
+        insetEnd="-2"
+        onClick={onClickClose}
+      />
+    </Alert.Root>
+  );
 
   return (
     <Container>
       <Heading>Heading</Heading>
-      <HStack align="start" gap="2rem" mb="2rem" width="100%">
+      {showAlert && AlertBox}
+      <HStack align="start" gap="2rem" my="2rem" width="100%">
         <TextInput
           id="first-name"
           label="First name"
@@ -54,13 +114,18 @@ function Home() {
           value={lastName}
         />
       </HStack>
-      <Button onClick={onSubmitHandler}>Submit</Button>
+      <ButtonGroup>
+        <Button onClick={onSubmitHandler}>Submit</Button>
+        <Button onClick={onClearHandler} variant="outline">
+          Clear
+        </Button>
+      </ButtonGroup>
       <VStack align="left" gap="2rem" mt="2rem" width="100%">
         <div>First name: {firstName}</div>
         <div>Last name: {lastName}</div>
       </VStack>
     </Container>
   );
-}
+};
 
 export default Home;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,19 +1,44 @@
 // import { useState } from "react";
+import styled from "styled-components";
 import { Outlet, Link } from "react-router-dom";
+
+const StyledNav = styled.div`
+  padding: 1rem;
+  nav {
+    font-size: 12px;
+    font-weight: 500;
+    margin: auto;
+    max-width: 1280px;
+    text-transform: uppercase;
+    width: 100%;
+    ul {
+      display: flex;
+      justify-content: center;
+      li {
+        padding: 0.5rem 1rem;
+        &:hover {
+          background-color: rgba(255, 255, 255, 0.1);
+        }
+      }
+    }
+  }
+`;
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/form">Form</Link>
-          </li>
-        </ul>
-      </nav>
+      <StyledNav>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </nav>
+      </StyledNav>
 
       <Outlet />
     </>
